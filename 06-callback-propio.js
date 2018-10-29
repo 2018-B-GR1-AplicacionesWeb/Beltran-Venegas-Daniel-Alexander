6c528b1ccbdc0730ec097eb9e7c44d8203573bb0
DanielAlexanderBeltran <danbelven@gmail.com>
2018-10-29 11:40:33 -0500

    Update 06-callback-propio.js

--- 06-callback-propio.js
+++ 06-callback-propio.js
@@ -46,29 +46,94 @@ appendFile('06-texto.txt',
     }
 );
 
-// promesa openfile
-nuevaPromesaOppen
-    .then(
-        (contenidoArchivo) => {
-            console.log('Todo bien', contenidoArchivo);
-            return nuevaPromesaOppen(contenidoArchivo)
+const nuevaPromesaAppendFile = (nombreArchivo, contenidoArchivo) => {
+    return new Promise(
+        (resolve, reject) => {
+            fs.readFile(nombreArchivo,'utf-8',
+                (error,contenidoArchivoLeido)=> {
+                    if (error) {
+                        fs.writeFile(nombreArchivo, contenidoArchivo,
+                            (err) => {
+                                if (err) {
+                                    reject(console.error('Error'));
+                                } else {
+                                    resolve(contenidoArchivo);
+                                }
+                            }
+                        );
+
+                    } else {
+                        fs.writeFile(nombreArchivo, contenidoArchivoLeido + contenidoArchivo,
+                            (err) => {
+                                if (err) {
+                                    reject(console.error('Error'));
+                                } else {
+                                    resolve(contenidoArchivo);
+                                }
+                            }
+                        );
+                    }
+                }
+            );
         }
-    )
+    );
+};
+
+nuevaPromesaAppendFile
     .then(
-        (contenidoCompleto) => {
-            console.log('Contenido completo', contenidoCompleto);
+        () => {
+            console.log('Todo bien');
+            return nuevaPromesaAppendFile(contenidoArchivo);
         }
     )
     .catch(
-        (resultadoError) => {
+        () => {
             console.log('Algo malo paso', resultadoError);
         }
     );
+//Ejercicio planteado en clases
+const nuevaPromesaAppendFile = (nombreArchivo, contenidoArchivo) => {
+    return new Promise(
+        (resolve, reject) => {
+            fs.readFile(nombreArchivo,'utf-8',
+                (error,contenidoArchivoLeido)=> {
+                    if (error) {
+                        fs.writeFile(nombreArchivo, contenidoArchivo,
+                            (err) => {
+                                if (err) {
+                                    reject(console.error('Error'));
+                                } else {
+                                    resolve(contenidoArchivo);
+                                }
+                            }
+                        );
 
-appendFile('06-texto.txt',
-    '\n Adios mundo',
-    () => {
+                    } else {
+                        fs.writeFile(nombreArchivo, contenidoArchivoLeido + contenidoArchivo,
+                            (err) => {
+                                if (err) {
+                                    reject(console.error('Error'));
+                                } else {
+                                    resolve(contenidoArchivo);
+                                }
+                            }
+                        );
+                    }
+                }
+            );
+        }
+    );
+};
 
-    }
-);
-
+nuevaPromesaAppendFile
+    .then(
+        () => {
+            console.log('Esta funcionando correctamente');
+            return nuevaPromesaAppendFile(contenidoArchivo);
+        }
+    )
+    .catch(
+        () => {
+            console.log('Error algo ocurrio', resultadoError);
+        }
+    );
